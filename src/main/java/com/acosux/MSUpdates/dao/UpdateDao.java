370ab8402b71497494735bf5f933b8a379fd7a18
jtabango <jtabango@obinte.com>
2022-08-02 11:24:39 -0500

    Versionamiento
    Se arreglo el listado de versiones

--- src/main/java/com/acosux/MSUpdates/dao/UpdateDao.java
+++ src/main/java/com/acosux/MSUpdates/dao/UpdateDao.java
@@ -6,7 +6,7 @@
 package com.acosux.MSUpdates.dao;
 
 import com.acosux.MSUpdates.util.UpdateTO;
-import com.acosux.MSUpdates.util.ComboGenericoTO;
+import com.acosux.MSUpdates.util.VersionTO;
 import com.acosux.MSUpdates.util.dao.GenericDao;
 import java.util.List;
 
@@ -14,6 +14,6 @@ public interface UpdateDao extends GenericDao<UpdateTO, Integer> {
 
     public List <UpdateTO> buscarActualizacionesPorVersion(String version) throws Exception;
     
-    public List <ComboGenericoTO> listarVersiones() throws Exception;
+    public List <VersionTO> listarVersiones() throws Exception;
 
 }
---
+++ src/main/java/com/acosux/MSUpdates/util/VersionTO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acosux.MSUpdates.util;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author jtabango
+ */
+
+@Entity
+public class VersionTO implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    private String act_version;
+
+    public VersionTO() {
+    }
+
+    public String getAct_version() {
+        return act_version;
+    }
+
+    public void setAct_version(String act_version) {
+        this.act_version = act_version;
+    }
+
+   
+ 
+}
